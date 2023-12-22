e36193a824bfa15ec2f633e1dec4768b7dff066d
ArtemT12345 <153588244+ArtemT12345@users.noreply.github.com>
2023-12-22 11:39:57 +0300

    Add files via upload

---
+++ lr 3-4/15.pas
@@ -0,0 +1,29 @@
+﻿program zad15;
+var
+  a, b, c, d: longint;
+begin
+  
+  readln(a);
+  a := abs(a);
+  b := 0;
+  c := 1;
+  repeat
+    d := a mod 10 ;
+    b := b + d;
+    c := c * d;
+    a := a div 10
+  until a = 0;
+  writeln( b);
+  writeln( c);
+  Readln(a);
+  if a /10 > 1 then writeln(2);
+  if a /100 > 1 then writeln(3);
+  if a /1000 > 1 then writeln(4);
+  if a /10000 > 1 then writeln(5);
+  if a /100000 > 1 then writeln(6);
+  if a /1000000 > 1 then writeln(7);
+  if a /10000000 > 1 then writeln(8);
+  
+  
+  readln
+end.
---
+++ lr 3-4/6.pas
@@ -0,0 +1,18 @@
+﻿program zad13;
+Var
+
+  i:integer;
+  a,b:real;
+begin
+  i:=3;
+  while i<=60 do 
+      
+   begin
+  i:=i+6;
+    a:=a+i
+    
+  end;
+  write (a/10,' ');
+end.
+
+
